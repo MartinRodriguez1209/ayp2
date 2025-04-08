969a003f1347cb1376f16a81db5e6023f1c41a0f
martinrodriguez1209 <martinrodriguez.1209@gmail.com>
2025-04-08 18:57:13 -0300

    ej8

---
+++ practico 2/ejercicio 9/src/vectores/vector.adb
@@ -0,0 +1,69 @@
+package body vector is
+   procedure Leer (Vec : out Vecgeneric) is
+   begin
+      for I in Vec'range loop
+         Get (Vec (I));
+      end loop;
+   end Leer;
+
+   procedure Imprimir (vec : in vecgeneric) is
+   begin
+      for I in Vec'range loop
+         Put (Vec (I));
+      end loop;
+   end Imprimir;
+
+   procedure Busqueda
+     (vec        : in vecgeneric;
+      A          : in Tipodato;
+      encontrado : out boolean;
+      posi       : out integer) is
+   begin
+
+      Encontrado := False;
+      for I in Vec'range loop
+         if Vec (I) = A then
+            Encontrado := True;
+            Posi := integer (i);
+         end if;
+      end loop;
+   end Busqueda;
+
+   procedure Ordenamiento (Vec : in out Vecgeneric) is
+      Aux : Tipodato;
+   begin
+      for I in indice'range loop
+         for J in indice'first .. indice'pred (indice'last) loop
+
+            if vec (j) > vec (indice'succ (j)) then
+               Aux := Vec (J);
+               Vec (J) := Vec (indice'succ (j));
+               Vec (indice'succ (j)) := Aux;
+            end if;
+
+         end loop;
+      end loop;
+   end Ordenamiento;
+
+   procedure comparacion (vec1, vec2 : in vecgeneric; compa : out boolean) is
+      Acum1, Acum2 : Tipodato := valorinicial;
+      raiz1, raiz2 : float;
+   begin
+
+      for I in Indice'range loop
+         Acum1 := Vec1 (I) * vec1 (i) + Acum1;
+         Acum2 := Vec2 (I) * vec2 (i) + Acum2;
+      end loop;
+
+      raiz1 := Raiz (acum1);
+      raiz2 := Raiz (acum2);
+
+      if Raiz1 = Raiz2 then
+         compa := true;
+      else
+         Compa := False;
+      end if;
+
+   end comparacion;
+
+end vector;
---
+++ practico 2/ejercicio 9/src/vectores/vector.ads
@@ -0,0 +1,21 @@
+generic 
+   type Tipodato is private;
+   type Indice is range <> ;
+   Valorinicial: tipodato;
+   with function "+"(A,B: in Tipodato) return Tipodato;
+   with function "*"(A,B: in Tipodato) return Tipodato;
+   with function "-"(A,B: in Tipodato) return Tipodato;
+   with function ">"(A,B: in Tipodato) return Boolean;
+   with function Raiz(A: in Tipodato) return float;
+   with procedure Put(A: in Tipodato);
+   with procedure get(A: out tipodato);
+   
+   package Crearpackage is 
+     type VecGeneric is array(Indice) of Tipodato;
+      procedure Leer(Vec: out Vecgeneric);
+      procedure Imprimir (Vec: in Vecgeneric);
+      procedure Busqueda(Vec: in Vecgeneric; A: in Tipodato; Encontrado: out Boolean; Posi: out Integer);
+      procedure Ordenamiento(Vec: in out Vecgeneric);
+      procedure comparacion( vec1, vec2: in vecgeneric; compa: out boolean  );
+   end Crearpackage;
+   
