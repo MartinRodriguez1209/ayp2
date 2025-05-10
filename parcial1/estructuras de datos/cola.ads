c53fbe1ae8c0a4ccf0efb8361ed2044f33acd819
martinrodriguez1209 <martinrodriguez.1209@gmail.com>
2025-05-10 18:02:40 -0300

    cola dinamcia

---
+++ parcial1/estructuras de datos/cola.ads
@@ -0,0 +1,18 @@
+generic
+   type tipoElem is private;
+package cola is
+
+   function Vacia (Cola : in TipoCola) return Boolean;
+   procedure Inscola (Cola : in out TipoCola; Elemento : in TipoElem);
+   procedure Supcola (Cola : in out TipoCola; Elemento : out TipoElem);
+   procedure Limpiar (Cola : in out TipoCola);
+   type TipoNodo;
+   type TipoPun is access TipoNodo;
+   type TipoNodo is record
+      Info : TipoElem;
+      Sig  : TipoPun;
+   end record;
+   type TipoCola is record
+      Frente, Final : TipoPun;
+   end record;
+end cola;
