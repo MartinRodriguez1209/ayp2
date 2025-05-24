f48f9e242ae215fe44a778661b05e350187d2415
Matias Zahn <zahnmatias14@gmail.com>
2025-05-24 16:26:42 -0300

    TAD mecanico agregado

---
+++ Trabajo Final/Prueba/src/mecanico.adb
@@ -0,0 +1,92 @@
+package body Mecanico is
+   
+   function Crearmecanico( nombre, apellido, especialidad: string; dni: natural ) return Tipomecanico is
+      M: Tipomecanico;
+      
+      function Calcularstring(Palabra: String) return Natural is
+         Len: Natural:= 0;
+      begin
+         for I in Palabra'range loop
+            if palabra(I) /= ' ' then 
+               Len:= Len + 1;
+            end if;
+         end loop;
+         return len; 
+      end calcularstring;
+
+   begin
+      
+      if  not (Dni  in 10_000_000 .. 99_999_999) then 
+         raise Dni_Invalido with "El dni debe contener exctamente 8 digitos. ";
+      end if;
+      
+
+      M.Nombrelen:= Calcularstring(Nombre);
+      M.Apellidolen:= Calcularstring(Apellido);
+      m.especialidadlen:= Calcularstring(especialidad);
+
+
+      m.nombre(1..nombre'length):= nombre;
+      m.apellido(1..apellido'length):= apellido;
+      M.Especialidad(1..Especialidad'length):= Especialidad;
+      m.dni:= dni;
+
+      return M;
+   end Crearmecanico;
+   
+   --En el programa principal se debera elegir la rta y despues rellenar una nueva variable "NUEVOSDATOS" donde esten los campos a cambiar
+   procedure Modificarmecanico(M: in out Tipomecanico; nuevosDatos: tipomecanico; rta: integer) is
+   begin
+      if Rta = 1 then --MODIFICAR NOMBRE 
+         M.Nombre:= Nuevosdatos.Nombre;
+         m.nombrelen:= nuevosDatos.nombreLen;
+      else if Rta = 2 then
+         M.apellido:= Nuevosdatos.apellido;
+         m.apellidolen:= nuevosDatos.apellidoLen;         
+      else
+         M.apellido:= Nuevosdatos.apellido;
+         M.Apellidolen:= Nuevosdatos.Apellidolen;
+         M.nombre:= Nuevosdatos.nombre;
+         m.nombrelen:= nuevosDatos.nombreLen;
+         end if;
+      end if;
+      
+   end Modificarmecanico;
+   
+   
+   procedure Bajamecanico(M: in out Tipomecanico ) is
+   begin
+      M.Estado:= False;
+   end Bajamecanico;
+   
+   
+
+
+   function Obtenernombre(M: Tipomecanico) return String is
+   begin
+      return M.Nombre(1..M.Nombrelen);
+   end Obtenernombre;
+      
+
+   function Obtenerapellido(M: Tipomecanico) return String is
+   begin
+      return M.Apellido(1..M.Apellidolen);
+   end Obtenerapellido;
+   
+   function Obtenerespecialidad(M: Tipomecanico) return String is
+   begin
+      return M.Especialidad(1..M.Especialidadlen);
+   end Obtenerespecialidad;
+     
+
+   function Obtenerdni(M: Tipomecanico) return Natural is 
+   begin
+      
+      return M.Dni;
+   end Obtenerdni;
+   
+
+
+
+end Mecanico;
+
---
+++ Trabajo Final/Prueba/src/mecanico.ads
@@ -0,0 +1,31 @@
+package Mecanico is
+   type Tipomecanico is private;
+   Dni_Invalido: exception;
+   
+   function Crearmecanico( nombre, apellido, especialidad: string; dni: natural) return Tipomecanico;
+   procedure Modificarmecanico(M: in out Tipomecanico; nuevosDatos: tipomecanico; rta: integer);
+   procedure Bajamecanico(M: in out Tipomecanico);
+   
+
+   --FUNCIONES PARA OBTENER LOS DATOS DEL MECANICO 
+   function Obtenernombre(M: Tipomecanico) return String;
+   function Obtenerapellido(M: Tipomecanico) return String;
+   function Obtenerespecialidad(M: Tipomecanico) return String;
+   function Obtenerdni(M: Tipomecanico) return Natural;
+   
+   
+   private
+   subtype String50 is String(1..50);
+   type Tipomecanico is record
+      Nombre: String50;
+      apellido: String50;
+      Especialidad: String50;
+      Nombrelen: natural;
+      Apellidolen: Natural;
+      Especialidadlen: Natural;
+      Dni: Natural;
+      estado: boolean;
+   end record;
+end Mecanico;
+
+      
