0bcc859b0be294937aeedadd9eabf1f42bc45ab1
martinrodriguez1209 <martinrodriguez.1209@gmail.com>
2025-05-25 23:19:40 -0300

    paquete mecanico

--- Trabajo Final/Prueba/src/mecanico.adb
+++ Trabajo Final/Prueba/src/mecanico.adb
@@ -1,92 +1,86 @@
 package body Mecanico is
-   
-   function Crearmecanico( nombre, apellido, especialidad: string; dni: natural ) return Tipomecanico is
-      M: Tipomecanico;
-      
-      function Calcularstring(Palabra: String) return Natural is
-         Len: Natural:= 0;
+
+   function Crearmecanico
+     (nombre, apellido, especialidad : string; dni : natural)
+      return Tipomecanico
+   is
+      M : Tipomecanico;
+
+      function Calcularstring (Palabra : String) return Natural is
+         Len : Natural := 0;
       begin
          for I in Palabra'range loop
-            if palabra(I) /= ' ' then 
-               Len:= Len + 1;
+            if palabra (I) /= ' ' then
+               Len := Len + 1;
             end if;
          end loop;
-         return len; 
+         return len;
       end calcularstring;
 
    begin
-      
-      if  not (Dni  in 10_000_000 .. 99_999_999) then 
-         raise Dni_Invalido with "El dni debe contener exctamente 8 digitos. ";
+
+      if not (Dni in 10_000_000 .. 99_999_999) then
+         raise Dni_Invalido;
       end if;
-      
 
-      M.Nombrelen:= Calcularstring(Nombre);
-      M.Apellidolen:= Calcularstring(Apellido);
-      m.especialidadlen:= Calcularstring(especialidad);
+      M.Nombrelen := Calcularstring (Nombre);
+      M.Apellidolen := Calcularstring (Apellido);
+      m.especialidadlen := Calcularstring (especialidad);
 
-
-      m.nombre(1..nombre'length):= nombre;
-      m.apellido(1..apellido'length):= apellido;
-      M.Especialidad(1..Especialidad'length):= Especialidad;
-      m.dni:= dni;
+      m.nombre (1 .. nombre'length) := nombre;
+      m.apellido (1 .. apellido'length) := apellido;
+      M.Especialidad (1 .. Especialidad'length) := Especialidad;
+      m.dni := dni;
 
       return M;
    end Crearmecanico;
-   
+
    --En el programa principal se debera elegir la rta y despues rellenar una nueva variable "NUEVOSDATOS" donde esten los campos a cambiar
-   procedure Modificarmecanico(M: in out Tipomecanico; nuevosDatos: tipomecanico; rta: integer) is
+   procedure Modificarmecanico
+     (M : in out Tipomecanico; nuevosDatos : tipomecanico; rta : integer) is
    begin
-      if Rta = 1 then --MODIFICAR NOMBRE 
-         M.Nombre:= Nuevosdatos.Nombre;
-         m.nombrelen:= nuevosDatos.nombreLen;
-      else if Rta = 2 then
-         M.apellido:= Nuevosdatos.apellido;
-         m.apellidolen:= nuevosDatos.apellidoLen;         
+      if Rta = 1 then
+         --MODIFICAR NOMBRE
+         M.Nombre := Nuevosdatos.Nombre;
+         m.nombrelen := nuevosDatos.nombreLen;
       else
-         M.apellido:= Nuevosdatos.apellido;
-         M.Apellidolen:= Nuevosdatos.Apellidolen;
-         M.nombre:= Nuevosdatos.nombre;
-         m.nombrelen:= nuevosDatos.nombreLen;
+         if Rta = 2 then
+            M.apellido := Nuevosdatos.apellido;
+            m.apellidolen := nuevosDatos.apellidoLen;
+         else
+            M.apellido := Nuevosdatos.apellido;
+            M.Apellidolen := Nuevosdatos.Apellidolen;
+            M.nombre := Nuevosdatos.nombre;
+            m.nombrelen := nuevosDatos.nombreLen;
          end if;
       end if;
-      
+
    end Modificarmecanico;
-   
-   
-   procedure Bajamecanico(M: in out Tipomecanico ) is
+
+   procedure Bajamecanico (M : in out Tipomecanico) is
    begin
-      M.Estado:= False;
+      M.Estado := False;
    end Bajamecanico;
-   
-   
 
-
-   function Obtenernombre(M: Tipomecanico) return String is
+   function Obtenernombre (M : Tipomecanico) return String is
    begin
-      return M.Nombre(1..M.Nombrelen);
+      return M.Nombre (1 .. M.Nombrelen);
    end Obtenernombre;
-      
 
-   function Obtenerapellido(M: Tipomecanico) return String is
+   function Obtenerapellido (M : Tipomecanico) return String is
    begin
-      return M.Apellido(1..M.Apellidolen);
+      return M.Apellido (1 .. M.Apellidolen);
    end Obtenerapellido;
-   
-   function Obtenerespecialidad(M: Tipomecanico) return String is
-   begin
-      return M.Especialidad(1..M.Especialidadlen);
-   end Obtenerespecialidad;
-     
 
-   function Obtenerdni(M: Tipomecanico) return Natural is 
+   function Obtenerespecialidad (M : Tipomecanico) return String is
    begin
-      
+      return M.Especialidad (1 .. M.Especialidadlen);
+   end Obtenerespecialidad;
+
+   function Obtenerdni (M : Tipomecanico) return Natural is
+   begin
+
       return M.Dni;
    end Obtenerdni;
-   
-
-
 
 end Mecanico;
-
