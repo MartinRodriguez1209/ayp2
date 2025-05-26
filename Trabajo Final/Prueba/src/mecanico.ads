6c2c01bef796304c7e3c50fff9e2f35d1c0a432f
Matias Zahn <zahnmatias14@gmail.com>
2025-05-25 23:37:16 -0300

    Tad mecanico: ModificarMecanico modificado para usar parametro con string

--- Trabajo Final/Prueba/src/mecanico.ads
+++ Trabajo Final/Prueba/src/mecanico.ads
@@ -1,31 +1,30 @@
 package Mecanico is
    type Tipomecanico is private;
-   Dni_Invalido: exception;
-   
-   function Crearmecanico( nombre, apellido, especialidad: string; dni: natural) return Tipomecanico;
-   procedure Modificarmecanico(M: in out Tipomecanico; nuevosDatos: tipomecanico; rta: integer);
-   procedure Bajamecanico(M: in out Tipomecanico);
-   
+   Dni_Invalido : exception;
 
-   --FUNCIONES PARA OBTENER LOS DATOS DEL MECANICO 
-   function Obtenernombre(M: Tipomecanico) return String;
-   function Obtenerapellido(M: Tipomecanico) return String;
-   function Obtenerespecialidad(M: Tipomecanico) return String;
-   function Obtenerdni(M: Tipomecanico) return Natural;
-   
-   
-   private
-   subtype String50 is String(1..50);
+   function Crearmecanico
+     (nombre, apellido, especialidad : string; dni : natural)
+      return Tipomecanico;
+   procedure Modificarmecanico
+     (M : in out Tipomecanico; nuevoValor : string; rta : integer);
+   procedure Bajamecanico (M : in out Tipomecanico);
+
+   --FUNCIONES PARA OBTENER LOS DATOS DEL MECANICO
+   function Obtenernombre (M : Tipomecanico) return String;
+   function Obtenerapellido (M : Tipomecanico) return String;
+   function Obtenerespecialidad (M : Tipomecanico) return String;
+   function Obtenerdni (M : Tipomecanico) return Natural;
+
+private
+   subtype String50 is String (1 .. 50);
    type Tipomecanico is record
-      Nombre: String50;
-      apellido: String50;
-      Especialidad: String50;
-      Nombrelen: natural;
-      Apellidolen: Natural;
-      Especialidadlen: Natural;
-      Dni: Natural;
-      estado: boolean;
+      Nombre          : String50;
+      apellido        : String50;
+      Especialidad    : String50;
+      Nombrelen       : natural;
+      Apellidolen     : Natural;
+      Especialidadlen : Natural;
+      Dni             : Natural;
+      estado          : boolean;
    end record;
 end Mecanico;
-
-      
